19a3e7709694a447a3b6ffd953fc957a1aabcc44
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-10-18 18:00:26 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// const express = require('express');
+// const React = require('react');
+// const renderToString = require('react-dom/server').renderToString;
+// const Home = require('./client/components/home').default;
+
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import Home from './client/components/home';
+
+const app = express();
+
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+	const content = renderToString(<Home />)
+
+	const html = `
+		<html>
+			<head></head>
+			<body>
+				<div>${content}</div>
+				<script src="bundle.js"></script>
+			</body>
+		</html>
+	`;
+
+	res.send(html);
+});
+
+app.listen(3000, () => {
+	console.log('listening on port 3000')
+});
